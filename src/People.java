356902b79de276ca39a5b91a51fa7335bf695b77
tiyjastrow <tiyjastrow@users.noreply.github.com>
2016-09-10 17:54:40 -0500

    Update People.java

--- src/People.java
+++ src/People.java
@@ -1,5 +1,6 @@
 /**
- * Created by zach on 10/19/15.
+ * Reads a csv file and maps country name to a list of people who are from that country. 
+ * Then, for each list sorts by last name.
  */
 public class People {
     public static void main(String[] args) {
